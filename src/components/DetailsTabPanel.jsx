146c25129ca8101d65f9a39a76e05fc297949cee
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2021-05-15 23:40:14 +0200

    Code refactored

--- src/components/DetailsTabPanel.jsx
+++ src/components/DetailsTabPanel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -33,7 +33,7 @@ TabPanel.propTypes = {
   value: PropTypes.any.isRequired,
 };
 
-function a11yProps(index) {
+function allyProps(index) {
   return {
     id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
@@ -42,6 +42,12 @@ function a11yProps(index) {
 const convertNumberToDayString = (index) => {
   return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][index] || ''
 }
+const getTodayString = (item)=>{
+  let date = new Date(item.dt * 1000)
+  let todayString = `${convertNumberToDayString(date.getDay())} at ${date.getHours() - 1}:00`;
+  return todayString;
+}
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -58,7 +64,7 @@ const useStyles = makeStyles((theme) => ({
 
 const DetailsTabPanel = (props) => {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -68,12 +74,10 @@ const DetailsTabPanel = (props) => {
   if (props.todaysDetails) {
     //First Tab content
     todayItemsList = props.todaysDetails.map((item, index) => {
-      let date = new Date(item.dt * 1000)
-      let todayString = `${convertNumberToDayString(date.getDay())} at ${date.getHours() - 1}:00`;
       return (
         <DetailsItem
           key={index}
-          todayString={todayString}
+          todayString={getTodayString(item)}
           temp={item.main.temp.toFixed(0)}
           iconURL={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
         />
@@ -83,13 +87,10 @@ const DetailsTabPanel = (props) => {
 
     //Second Tab content
     tomorrowItemsList = props.tomorrowDetails.map((item, index) => {
-      let date = new Date(item.dt * 1000)
-      let todayString = `${convertNumberToDayString(date.getDay())} at ${date.getHours() - 1}:00`;
-
       return (
         <DetailsItem
           key={index * 12}
-          todayString={todayString}
+          todayString={getTodayString(item)}
           temp={item.main.temp.toFixed(0)}
           iconURL={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
         />
@@ -100,15 +101,13 @@ const DetailsTabPanel = (props) => {
 
     //Third Tab content
     nextDaysItemsList = props.nextDays.map((item, index) => {
-      let date = new Date(item.dt * 1000)
       let validHours = [5,6,7,14,15,16]
+      let date = new Date(item.dt * 1000) 
       if (validHours.includes(date.getHours())) {
-        let todayString = `${convertNumberToDayString(date.getDay())} ${date.getHours() - 1}:00 `;
-        console.log(todayString,item.main.temp)
         return (
           <DetailsItem
             key={index * 133}
-            todayString={todayString}
+            todayString={getTodayString(item)}
             temp={item.main.temp.toFixed(0)}
             iconURL={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
           />
@@ -117,26 +116,17 @@ const DetailsTabPanel = (props) => {
     })
     //End Third Tab content
 
+    let itemsList = [todayItemsList,tomorrowItemsList,nextDaysItemsList]
     return (
       <div className={classes.root}>
         <AppBar position="static">
           <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-            <Tab label="Today" {...a11yProps(0)} />
-            <Tab label="Tomorrow" {...a11yProps(1)} />
-            <Tab label="Next days" {...a11yProps(2)} />
+            <Tab label="Today" {...allyProps(0)} />
+            <Tab label="Tomorrow" {...allyProps(1)} />
+            <Tab label="Next days" {...allyProps(2)} />
           </Tabs>
         </AppBar>
-        <TabPanel value={value} index={0}>
-          {todayItemsList}
-        </TabPanel>
-
-        <TabPanel value={value} index={1}>
-          {tomorrowItemsList}
-        </TabPanel>
-
-        <TabPanel value={value} index={2}>
-          {nextDaysItemsList}
-        </TabPanel>
+        {itemsList.map((item,index)=><TabPanel value={value} index={index}>{item}</TabPanel>)}
       </div>
     );
   } else {
